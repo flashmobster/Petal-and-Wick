76dbc0c260d1a696aab07e9edca3e5af4be8b34a
Maya Montague <mayamontague4@gmail.com>
2024-05-13 15:21:37 -0500

    .

--- backend/candleSchema.js
+++ backend/candleSchema.js
@@ -18,26 +18,11 @@ const candleSchema = new mongoose.Schema({
 
 const Candle = mongoose.model('Candle', {candleSchema} )
 
-const flowerSchema = new mongoose.Schema({
-    name:{
-        type: String
-    },
-    description:{
-        type: String
-    },
-    img_url:{
-        type: String
-    },
-    price:{
-        type: Number
-    }
-});
 
-const Flower = mongoose.model('Flower', {flowerSchema} )
 
 mongoose.connect(Connection_String, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Error connecting to MongoDB:', err));
 
-module.exports = Flower;
+
 module.exports = Candle;
