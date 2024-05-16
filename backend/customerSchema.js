3174be46c1835e8580cf834970e80f974a6e89ea
Maya Montague <mayamontague4@gmail.com>
2024-05-16 12:18:44 -0500

    .

--- backend/customerSchema.js
+++ backend/customerSchema.js
@@ -16,10 +16,20 @@ const customerSchema = new mongoose.Schema({
 
 const Customer = mongoose.model('Customer', {customerSchema} )
 
-mongoose.connect(Connection_String)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Error connecting to MongoDB:', err));
+const customerArray = [  new Customer({ name: 'John Doe', email: 'null@gmail.com', phone: '1234567890' }),
+new Customer({ name: 'Jane Doe', email: 'null', phone: '0987654321' })
+]
 
+async function connect() {
+    try {
+        await mongoose.connect(Connection_String);
+        console.log('Connected to MongoDB');
+        await Customer.insertMany(customerArray);
+    } catch (err) {
+        console.error('Error connecting to MongoDB:', err);
+    }
+}
 
+connect();
 
 module.exports = Customer;    
--- backend/orderschema.js
+++ backend/orderschema.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const Connection_String = 'mongodb+srv://crishoyle:pfl3Pvny1AgCq60a@cluster0.qcxxxxi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-const { Schema } = mongoose;
 const Customer = require('./customerSchema');
+const Schema = mongoose.Schema;
 
-const orderSchema = new Schema({
+const orderSchema = new mongoose.Schema({
     name: {
         type: String
     },
@@ -15,12 +15,12 @@ const orderSchema = new Schema({
     },
     price: {
         type: Number
+    },
+    customer: {
+        type: Schema.Types.ObjectId,
+        ref: Customer
     }
-    // },
-    // customer: {
-    //     type: Schema.Types.ObjectId,
-    //     ref: 'Customer'
-    // }
+    
 });
 
 const Order = mongoose.model('Order', orderSchema);
