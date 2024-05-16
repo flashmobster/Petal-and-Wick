d7c6da41639ccc8ec02ba3cc80ab574298ec36be
flashmobster <crishoyle@gmail.com>
2024-05-16 13:59:54 -0500

    Revert "added cors"
    This reverts commit 3875e51604e50abc6f0663d8fd8e4b97c5b58e08.

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors');
-
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(cors());
-app.use(express.json())
 
 app.use(bodyParser.json());
 
--- frontend/GroupProject/candle.js
+++
@@ -1,5 +0,0 @@
-async function displayCandles() {
-    const response = await fetch("https://petal-and-wick.onrender.com/api/candles");
-    const candles = await response.json();
-    console.log(candles);
-  }
